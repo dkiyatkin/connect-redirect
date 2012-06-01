21fedeca890441cf89acac55a095c3df87ade13f
Dmitriy Kiyatkin <d.kiyatkin@itlf.ru>
2012-06-01 21:20:49 +0400

    init

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var regRedirection = function(regexp, newSubStr, type) {
+module.exports = function(options) {
 	return function(req, res, next) {
-		if (reg_urls.double_slash.test(req.url_parse.pathname)) {
-			var loc = req.url_parse.pathname.replace(reg_urls.double_slash, newSubStr) + req.url_parse.search;
-			console.log(newSubStr+'-redirect: ', req.url, loc);
-			if (type == 'p') {
-				res.moved(loc);
-			} else {
-				res.redirect(loc);
-			}
+		if (options.reg.test(req.url)) {
+			var loc = req.url.replace(options.reg, options.str);
+			console.log(options.str+'-redirect: ', req.url, loc);
+			res.writeHead(301, {'Location': loc});
+			res.end();
 		} else { next(); }
 	};
 };
